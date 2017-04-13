58b459acb7b889ed0713bdb3e5f7c63d1b7e930e
Joel <joel.e.camera@gmail.com>
2017-04-13 14:44:52 -0300

    bug corregido en wp

--- src/tp1/lib/wp.cpp
+++ src/tp1/lib/wp.cpp
@@ -3,15 +3,13 @@
 
 matriz generarVectorWP(map<int, Equipo>& equipos){
     double valorWp;
-    int i = 0;
     matriz wp(equipos.size(), vector<double> (1));
     for ( auto it = equipos.begin(); it != equipos.end(); ++it ){
         Equipo eq = it->second;
         if (eq.jugados != 0){
             valorWp = eq.ganados/eq.jugados;
-            wp[i][0] = valorWp;
+            wp[eq.index][0] = valorWp;
         }
-        i++;
     }
     return wp;
 }
